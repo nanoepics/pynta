608213fe72ef7106cfce5587164e37df1e5a108d
Aquiles Carattino <aquiles@aquicarattino.com>
2018-10-16 10:43:54 +0200

    Initial structure for the project

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='pynta',
+    version='0.0',
+    description='Python Nanoparticle Tracking Analysis',
+    packages=['pynta',
+              ],
+    url='https://github.com/nanoepics/pynta',
+    license='GPLv3',
+    author='Aquiles Carattino',
+    author_email='aquiles@aquicarattino.com',
+    classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+    ],
+    #package_data={'UUTrack': ['View/Monitor/Icons/*.*', 'View/Monitor/Icons/*.*']},
+    #include_package_data=True,
+    install_requires=['',]
+)
