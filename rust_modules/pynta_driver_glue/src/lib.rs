4d6915b5b454f47087c1b253cd1885a12776d044
Hayley Deckers <git@codingcat.nl>
2021-09-11 14:08:04 +0400

    add dummy Camera, dyn dispatch

--- rust_modules/pynta_driver_glue/src/lib.rs
+++ rust_modules/pynta_driver_glue/src/lib.rs
@@ -2,8 +2,7 @@ use pyo3::prelude::*;
 use pyo3::exceptions;
 use mcl_stagedrive::microdrive;
 use dcam4;
-use numpy::{ToPyArray, PyArray2};
-use ndarray::{ArrayD, ArrayViewD, ArrayViewMutD};
+use numpy::{PyArray2};
 
 struct Wrapper<T>(T);
 
@@ -22,34 +21,154 @@ struct PyStage {
     dev : microdrive::Device
 }
 
-#[pyclass(name="Camera")]
-struct PyCamera {
+#[pyclass(name="DcamCamera")]
+struct DcamCamera {
     dev : dcam4::Camera
 }
 
-#[pymethods]
-impl PyCamera{
-    #[new]
-    pub fn new() -> PyResult<Self> {
+impl DcamCamera{
+    fn new() -> PyResult<Self> {
         Ok(Self{
             dev: dcam4::Camera::new()
         })
     }
-    pub fn snap_into(&mut self, arr : &PyArray2<u16>) -> PyResult<()> {
-        to_py_err(self.dev.snap_into(unsafe{arr.as_slice_mut()}?))
-    }
-    pub fn stream_into(&mut self, arr : &PyArray2<u16>) -> PyResult<()> {
-        to_py_err(self.dev.stream_into(unsafe{arr.as_slice_mut()}?))
+}
+
+
+use std::sync::mpsc;
+use std::thread;
+
+#[pyclass(name="DummyCamera")]
+struct DummyCamera {
+    last_frame : usize,
+    join_handle : Option<thread::JoinHandle<()>>,
+    stop_signal : Option<mpsc::SyncSender<()>>
+}
+
+impl DummyCamera {
+    fn new() -> PyResult<Self> {
+        Ok(Self {
+            last_frame : 0,
+            join_handle : None,
+            stop_signal: None
+        })
     }
 
-    pub fn stop_stream(&mut self) -> PyResult<()> {
+    fn fill_buffer(&mut self, buffer : &mut [u16]) {
+        assert_eq!(buffer.len(), 2048*2048);
+        let phase_offset = (self.last_frame % 128) as f64 / 127.0;
+        for x in 0..2048{
+            let xf = (x as f64)/2047.0;
+            for y in 0..2048{
+                let yf = (y as f64)/2047.0;
+                let val = 350.0*(0.5+0.5*((xf+yf+phase_offset)*6.28).sin());
+                buffer[x*2048+y] = val.max(0.0) as u16;
+            }
+        }
+        self.last_frame += 1;
+    }
+}
+
+trait PyCamera{
+    // fn new() -> PyResult<Self>;
+    fn snap_into(&mut self, arr: &PyArray2<u16>) -> PyResult<()>;
+    fn stream_into(&mut self, arr: &PyArray2<u16>) -> PyResult<()>;
+    fn stop_stream(&mut self) -> PyResult<()>;
+}
+
+impl PyCamera for DcamCamera{
+    fn snap_into(&mut self, arr : &PyArray2<u16>) -> PyResult<()> {
+        to_py_err(self.dev.snap_into(unsafe{arr.as_slice_mut()?}))
+    }
+    fn stream_into(&mut self, arr : &PyArray2<u16>) -> PyResult<()> {
+        //does this buffer live long enough? should we store a copy of it somewhere in our memory to prevent python from GCing it till we're done with it too?
+        to_py_err(self.dev.stream_into(unsafe{arr.as_slice_mut()?}))
+    }
+
+    fn stop_stream(&mut self) -> PyResult<()> {
         to_py_err(self.dev.stop_stream())
     }
 }
 
+impl PyCamera for DummyCamera{
+    fn snap_into(&mut self, arr: &PyArray2<u16>) -> PyResult<()> {
+        Ok(self.fill_buffer(unsafe{arr.as_slice_mut()?}))
+    }
+    fn stream_into(&mut self, arr: &PyArray2<u16>) -> PyResult<()> {
+        self.stop_stream()?;
+        let (tx, rx) = mpsc::sync_channel::<()>(1);
+        self.stop_signal = Some(tx);
+        //very bad
+        let slice : &'static mut [u16] = unsafe{std::mem::transmute(arr.as_slice_mut()?)};
+        self.join_handle = Some(std::thread::spawn(move || {
+            let mut last_frame = 0;
+            while rx.try_recv().is_err() {
+                assert_eq!(slice.len(), 2048*2048);
+                let phase_offset = (last_frame % 128) as f64 / 127.0;
+                for x in 0..2048{
+                    let xf = (x as f64)/2047.0;
+                    for y in 0..2048{
+                        let yf = (y as f64)/2047.0;
+                        let val = 350.0*(0.5+0.5*((xf+yf+phase_offset)*6.28).sin());
+                        slice[x*2048+y] = val.max(0.0) as u16;
+                    }
+                }
+                last_frame += 1;
+                println!("finished frame {}", last_frame);
+                std::thread::sleep(std::time::Duration::from_millis(20));
+            }
+        }));
+        Ok(())
+    }
+    fn stop_stream(&mut self) -> PyResult<()> {
+        if self.join_handle.is_some() {
+            self.stop_signal.take()
+                .expect("Trying to stop a stream as the joinhandle exists but the stop sigal doesn't exist?")
+                .send(()).unwrap();
+            self.join_handle.take().unwrap().join().unwrap()
+        }
+        Ok(())
+    }
+}
 
+#[pyclass]
+pub struct Camera{
+    device : Box::<dyn PyCamera + Send>
+}
 
+#[pymethods]
+impl Camera{
+    #[new]
+    pub fn new(device : &str) -> PyResult<Self> {
+        match device.to_lowercase().as_str() {
+            "dcam" | "hamamatsu" => {
+                Ok(Self{
+                    device : Box::new(DcamCamera::new()?) as Box::<dyn PyCamera + Send>
+                })
+            },
+            "dummy" | "test" => {
+                Ok(Self{
+                    device : Box::new(DummyCamera::new()?) as Box::<dyn PyCamera + Send>
+                })
+            },
+            e => {
+                Err(exceptions::PyValueError::new_err(
+                    format!("No Camera called \"{}\" supported. Please check for typos, whether other programs using the camera right now, or if pynta_drivers was build with the right features.",e)
+                ))
+            }
+        }
+    }
 
+    fn snap_into(&mut self, arr: &PyArray2<u16>) -> PyResult<()> {
+        self.device.snap_into(arr)
+    }
+    fn stream_into(&mut self, arr: &PyArray2<u16>) -> PyResult<()> {
+        self.device.stream_into(arr)
+    }
+    fn stop_stream(&mut self) -> PyResult<()> {
+        self.device.stop_stream()
+    }
+}
 
 
 #[pymethods]
@@ -81,6 +200,6 @@ impl PyStage {
 fn _pynta_drivers(py: Python, m: &PyModule) -> PyResult<()> {
     // m.add_function(wrap_pyfunction!(double, m)?)?;
     m.add_class::<PyStage>()?;
-    m.add_class::<PyCamera>()?;
+    m.add_class::<Camera>()?;
     Ok(())
 }
--- stream_cam.py
+++ stream_cam.py
@@ -4,18 +4,15 @@ from matplotlib import animation
 import pynta_drivers
 
 buffer = np.zeros((2048,2048), dtype=np.uint16)
-cam = pynta_drivers.Camera()
+cam = pynta_drivers.Camera("Dummy")
 cam.stream_into(buffer)
 
 fig = plt.figure()
 im = plt.imshow(buffer, vmin=0, vmax=350)
 
 def animate(i):
-    # xi = i // ny
-    # yi = i % ny
-    # data[xi, yi] = 1
     im.set_data(buffer)
     return im
 
-anim = animation.FuncAnimation(fig, animate,interval=12)
+anim = animation.FuncAnimation(fig, animate,interval=4)
 plt.show()
