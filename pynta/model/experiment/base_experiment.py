d11d7429c3dacd26ee3e3169250a85117dc9d060
Aron Opheij <aron.opheij+github@gmail.com>
2022-03-04 15:18:29 +0100

    Updated ContinuousTracker to use customizable (larger) area. And moved many classed to base_experiment.py

--- pynta/model/experiment/base_experiment.py
+++ pynta/model/experiment/base_experiment.py
@@ -218,6 +218,190 @@ class BaseExperiment:
 
         self.publisher.stop()
 
+class DataPipeline:
+    def __init__(self, parent) -> None:
+        self.parent = parent
+        self.save_img = False
+        self.clear_process_img_func_list()
+
+    def set_save_img_func(self, func):
+        self.save_img_func = func
+        self.save_img = True
+
+    def unset_save_img_func(self):
+        self.save_img = False
+
+    def clear_process_img_func_list(self):
+        self.process_img_funcs = []
+        self.process_img_funcs_names = []
+
+    def add_process_img_func(self, funcs, name):
+        self.process_img_funcs.append(funcs)
+        self.process_img_funcs_names.append(name)
+
+    def remove_process_img_func(self, name):
+        try:
+            i = self.process_img_funcs_names.index(name)
+            self.process_img_funcs.pop(i)
+            self.process_img_funcs_names.pop(i)
+        except:
+            pass
+
+    def __call__(self, data):
+        self.parent.temp_image = data
+        if self.save_img:
+            self.save_img_func(data)
+
+        for funcs in self.process_img_funcs:
+            if type(funcs) is list:
+                new_data = data.copy()
+                for func in funcs:
+                    new_data = func(new_data)
+            else:
+                func(data)
 
 
+class SaveTracksToHDF5:
+    def __init__(self, aqcuisition_grp):
+        self.batching = 1024
+        self.write_index = 0
+        self.frame = 0
 
+        self.grp = aqcuisition_grp.create_group("Tracks")
+        #self.grp.attrs["diameter"] = self.diameter
+        self.grp.attrs["creation"]  = str(datetime.utcnow())
+
+        #self.locations_dataset = aqcuisition_grp.create_dataset("locations", shape=(8,self.batching), dtype=np.float32, maxshape=(8,None), chunks=(8, self.batching), compression='gzip')
+        self.x_dataset = self.grp.create_dataset("x", shape=(self.batching,), dtype=np.float32, maxshape=(None,), chunks=(self.batching,))
+        self.y_dataset = self.grp.create_dataset("y", shape=(self.batching,), dtype=np.float32, maxshape=(None,), chunks=(self.batching,))
+        self.i_dataset = self.grp.create_dataset("intensities", shape=(self.batching,), dtype=np.float32, maxshape=(None,), chunks=(self.batching,))
+        self.frame_dataset = self.grp.create_dataset("frames", shape=(self.batching,), dtype=np.uint64, maxshape=(None,), chunks=(self.batching,), compression='gzip')
+
+    def __call__(self, locations):
+        row_count = len(locations[0])
+        if row_count:
+            #print("found {} particles on frame {}".format(row_count, self.frame))
+            #todo: handle row_count >
+            old_size = self.x_dataset.shape[0]
+            #print("old size is {}".format(old_size))
+            while row_count + self.write_index > old_size:
+                self.x_dataset.resize((old_size+self.batching,))
+                self.y_dataset.resize((old_size+self.batching,))
+                self.i_dataset.resize((old_size+self.batching,))
+                self.frame_dataset.resize((old_size+self.batching,))
+                old_size += self.batching
+            #print("trying to write x={}".format(locations["x"]))
+            #print(locations["x"].shape)
+            self.x_dataset[self.write_index:self.write_index+row_count] = locations[0]
+            self.y_dataset[self.write_index:self.write_index+row_count] = locations[1]
+            self.i_dataset[self.write_index:self.write_index+row_count] = locations[2]
+            self.frame_dataset[self.write_index:self.write_index+row_count] = np.ones(row_count ,dtype= np.uint64)*self.frame
+            self.write_index += row_count
+        self.frame += 1
+        return locations
+
+class SaveImageToHDF5:
+    def __init__(self, aqcuisition_grp, device, stride = 1):
+        #self.dataset_writer = dataset_writer
+        self.stride = stride-1
+        self.counter = 0
+        size = tuple(device.get_size())
+        self.dataset_writer = aqcuisition_grp.create_dataset("Image", shape=(0,)+size, dtype=np.uint16, maxshape=(None,)+size, chunks=(1,)+size, compression='gzip')
+        self.dataset_writer.attrs["stride"] = stride
+        self.dataset_writer.attrs["creation"]  = str(datetime.utcnow())
+    def __call__(self, image):
+        if self.counter == 0:
+            # print("writting image to file..")
+            #self.dataset_writer.send(image)
+            dsize = self.dataset_writer.shape
+            self.dataset_writer.resize((dsize[0]+1,) + image.shape)
+            self.dataset_writer[-1,:] = image
+        # else:
+        #     print("Skipping image writting..")
+        self.counter = self.counter + 1 if self.counter < self.stride else 0
+        return image
+
+class SaveDaqToHDF5:
+    def __init__(self, aqcuisition_grp, device):
+        #self.dataset_writer = dataset_writer
+        self.counter = 0
+        self.dataset_writer = aqcuisition_grp.create_dataset("DAQ-input", shape=(0,)+device.get_size(), dtype=device.data_type, maxshape=(None,)+device.get_size(), chunks=(1,)+device.get_size(), compression='gzip')
+        # write out the signal generator settings.
+        #self.dataset_writer.attrs["stride"] = stride
+        self.dataset_writer.attrs["creation"]  = str(datetime.utcnow())
+    def __call__(self, data):
+        dsize = self.dataset_writer.shape
+        self.dataset_writer.resize((dsize[0]+1,) + dsize[1:])
+        self.dataset_writer[-1,:] = data
+        return data
+
+class SaveTriggerToHDF5:
+    def __init__(self, aqcuisition_grp, device, edge=1):
+        #self.dataset_writer = dataset_writer
+        self.counter = 0
+        self.device_size = device.get_size()
+        self.dataset_writer_daq = aqcuisition_grp.create_dataset("DAQ-input", shape=(0,)+self.device_size, dtype=device.data_type, maxshape=(None,)+self.device_size, chunks=(1,)+self.device_size, compression='gzip')
+        # self.dataset_writer_trigger = aqcuisition_grp.create_dataset("trigger", shape=(0,)+device.get_size(), dtype=device.data_type, maxshape=(None,)+device.get_size(), chunks=(1,)+device.get_size(), compression='gzip')
+        self.dataset_writer_trigger = aqcuisition_grp.create_dataset("trigger", shape=(0,), dtype=np.int64, maxshape=(None,), compression='gzip')
+        # write out the signal generator settings.
+        #self.dataset_writer.attrs["stride"] = stride
+        t0 = str(datetime.utcnow())
+        self.dataset_writer_daq.attrs["creation"]  = t0
+        self.dataset_writer_trigger.attrs["creation"]  = t0
+        self.dataset_writer_daq.attrs["frequency"]  = device._sample_freq
+        self.dataset_writer_trigger.attrs["frequency"]  = device._sample_freq
+        self.edge = edge
+        self.daq_frame_count = 0
+        self.previous_level = None
+    def add_finished_timestamp(self):
+        t_end = str(datetime.utcnow())
+        self.dataset_writer_daq.attrs["finished"]  = t_end
+        self.dataset_writer_trigger.attrs["finished"]  = t_end
+    def __call__(self, data):
+        dsize_daq = self.dataset_writer_daq.shape
+        dsize_trigger = self.dataset_writer_trigger.shape
+        self.dataset_writer_daq.resize((dsize_daq[0]+1,) + dsize_daq[1:])
+        self.dataset_writer_daq[-1,:] = data[0]
+
+        if self.previous_level is None:
+            self.previous_level = data[1][0]  # If it is the first "window", make up "previous level" (the same as the first datapoint, so it won't register as a value change)
+        # Note, the last value of the previous window is prepended in order to catch state changes that might occur right at the edge of a window
+        trig = (np.array([self.previous_level] + data[1]) > 1.6).astype(int)
+
+        # THIS LINE IS FOR TESTING WITHOUT ACTUAL TRIGGERS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        # trig = (np.array([0,0,3,3,3,0,0,3,3,0,0]) > 1.6).astype(int)
+
+        self.previous_level = data[1][-1]  # store the last value for the next iteration
+        trig_indices = np.where(np.diff(trig) == self.edge)[0] + self.daq_frame_count * self.device_size[0]  # The +1 is removed because one value is prepended to the array
+        self.daq_frame_count += 1
+        if len(trig_indices):
+            current_size = dsize_trigger[0]
+            self.dataset_writer_trigger.resize((current_size + len(trig_indices),))
+            self.dataset_writer_trigger[current_size:] = trig_indices
+
+
+        # self.dataset_writer_trigger.resize((dsize_trigger[0]+1,) + dsize_trigger[1:])
+        # self.dataset_writer_trigger[-1,:] = data[1]
+        return data[0]
+
+class FileWrangler:
+    def __init__(self, filename) -> None:
+        if not filename.endswith('.hdf5'):
+            filename += '.hdf5'
+        while os.path.exists(filename):
+            base_name = filename[:-5].split('_')
+            if base_name[-1].isnumeric():
+                base_name[-1] = str(int(base_name[-1]) + 1)
+            else:
+                base_name += '1'
+            filename = '_'.join(base_name) + '.hdf5'
+        self.filename = filename
+        self.file = h5py.File(filename,'w',  libver='latest')
+        self.file.attrs["creation"] = str(datetime.utcnow())
+
+    def start_new_aquisition(self):
+        #print("starting aq of {}".format(device))
+        device_grp = self.file.require_group('data')
+        aquisition_nr = len(device_grp.keys())
+        grp = device_grp.create_group("Acquisition_{}".format(aquisition_nr))
+        return grp
